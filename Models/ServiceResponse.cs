6d952653adea9a8b7fbc7816a42ba0fcac126ad8
Fcruzlopes <flopescontas@gmail.com>
2021-11-30 16:59:47 +0000

    Added proper service response with generics

---
+++ Models/ServiceResponse.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace dotnet_rpg.Models
+{
+    public class ServiceResponse<T>
+    {
+        public T Data { get; set; }
+        public bool Success { get; set; } = true;
+        public string Message { get; set; } = null;
+    }
+}

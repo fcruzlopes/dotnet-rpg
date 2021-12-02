a2543a46c2c7355d16f56eb5913528173e066dde
Fcruzlopes <flopescontas@gmail.com>
2021-12-02 12:21:44 +0000

    Added update and delete characters

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace dotnet_rpg.Services.CharacterService
             new Character(),
             new Character { Id = 1, Name = "Sam"}
         };
+        
         private readonly IMapper _mapper;
 
         public CharacterService(IMapper mapper)
@@ -27,14 +29,34 @@ namespace dotnet_rpg.Services.CharacterService
             Character character = _mapper.Map<Character>(newCharacter);
             character.Id = characters.Max(c => c.Id) + 1;
             characters.Add(character);
-            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c))
+                                             .ToList();
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id)
+        {
+            var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
+            try
+            {
+                Character character = characters.First(c => c.Id.Equals(id));
+                characters.Remove(character);
+                serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c))
+                                                 .ToList();
+            }
+            catch(InvalidOperationException nullRef)
+            {
+                serviceResponse.Message = $"The id of the object don't exist|{nullRef.StackTrace}";
+                serviceResponse.Success = false; 
+            }
             return serviceResponse;
         }
 
         public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c))
+                                             .ToList();
             return serviceResponse;
         }
 
@@ -43,6 +65,28 @@ namespace dotnet_rpg.Services.CharacterService
             var serviceResponse = new ServiceResponse<GetCharacterDto>();
             serviceResponse.Data = _mapper.Map<GetCharacterDto>(characters.FirstOrDefault(c => c.Id.Equals(id)));
             return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updateCharacter)
+        {
+            var serviceResponse = new ServiceResponse<GetCharacterDto>();
+            try
+            {
+                Character character = characters.FirstOrDefault(c => c.Id.Equals(updateCharacter.Id));
+                character.Name = updateCharacter.Name;
+                character.HitPoints = updateCharacter.HitPoints;
+                character.Strength = updateCharacter.Strength;
+                character.Defense = updateCharacter.Defense;
+                character.Intelligence = updateCharacter.Intelligence;
+                character.Class = updateCharacter.Class;
+            }
+            catch(NullReferenceException nullRef)
+            {
+                serviceResponse.Message = $"The id of the object don't exist|{nullRef.StackTrace}";
+                serviceResponse.Success = false;   
+            }
+            return serviceResponse;
+            
         }
     }
 }
